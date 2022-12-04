fec0a58b0d24e4ba33fe13393ec0ff1bd1476786
maxvanwijnen <maxvanwijnen@gmail.com>
2022-12-04 10:16:05 +0100

    home page

--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,28 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { PhotoshootTileMenu }from '../components/PhotoshootTileMenu/PhotoshootTileMenu';
-
+import ImgTxt2Column from "../components/content-section";
+import React from "react";
+import Link from "next/link";
 
 export default function Home() {
+    const testText =  <>Ben jij op zoek naar professionele fotografie in de regio Leiden, Noordwijk of de bollenstreek? Samen bespreken wij jouw wensen en denk ik met je mee zodat je trots kan zijn op het eindresultaat. Hierbij heb ik mij gespecialiseerd in <Link href="/fotoshoot/kinderfotografie" ><a >kinderfotografie</a></Link> , portretfotografie, famillieportretten en zwangersschapsfoto's</>;
   return (
 
-    <div className={styles.container}>
+    <section className={styles.container}>
+      <h1>Professionele portretfotografie & fotoshoots</h1>
 
-    </div>
+        <ImgTxt2Column
+            imgSource="/img/header/Kinderfotografie-header.webp"
+            cssClass="Kinderen"
+            imgAlt = "Kinderfotografie"
+            imgWidth={573}
+            imgHeight={384}
+            txtTitle="Kinderfotografie"
+            txtContent={testText}
+        />
+
+
+    </section>
   )
 }
