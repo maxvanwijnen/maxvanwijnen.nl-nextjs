2320cd7fb191c2558745150fbc54d89bcd3118bd
maxvanwijnen <maxvanwijnen@gmail.com>
2023-03-03 10:18:43 +0100

    loveshoot fix

--- pages/fotoshoot/loveshoot.js
+++ pages/fotoshoot/loveshoot.js
@@ -1,16 +1,17 @@
-import ImgTxt2Column from "../../components/content-section";
+import Txt from "../../components/content-section";
 import React from "react";
+import TxtImg2Column from "../../components/TxtImg2Column/TxtImg2Column";
 
 export default function Loveshoot (){
     return (
-        <ImgTxt2Column
+        <TxtImg2Column
             imgSource="/img/header/Kinderfotografie-header.webp"
-            cssClass="Loveshoot"
-            imgAlt = "Loveshoot"
+            cssClass="Kinderen"
+            imgAlt = "Kinderfotografie"
             imgWidth={573}
             imgHeight={384}
-            txtTitle="Loveshoot"
-            txtContent="Zin in een leuke fotosessie met je geliefde? Gewoon omdat het leuk is, of misschien wel omdat jullie verloofd zijn of voor de uitnodiging van jullie bruiloft. Alles kan en alles mag! een loveshoot is natuurlijk ook het perfecte cadeau voor valentijnsdag!"
-        />
+            txtTitle="Kinderfotografie">
+            Zin in een leuke fotosessie met je geliefde? Gewoon omdat het leuk is, of misschien wel omdat jullie verloofd zijn of voor de uitnodiging van jullie bruiloft. Alles kan en alles mag! een loveshoot is natuurlijk ook het perfecte cadeau voor valentijnsdag!
+        </TxtImg2Column>
     );
 }
