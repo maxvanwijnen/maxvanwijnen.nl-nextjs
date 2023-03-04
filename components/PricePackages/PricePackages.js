1d7b699fc95a38ad27ad4cad15e4f8fbf963fa08
maxvanwijnen <maxvanwijnen@gmail.com>
2023-03-04 15:26:20 +0100

    escaped quotes

--- components/PricePackages/PricePackages.js
+++ components/PricePackages/PricePackages.js
@@ -12,7 +12,7 @@ const PricePackages = () => {
                     uspList={["5 Foto's","Hoge kwaliteit nabewerkt", "Digitale download"]}
                     img="https://www.maxvanwijnen.nl/www/images/kinderen/Fotoshoot-strand-kind.webp"
                 >
-                    Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto's die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto\'s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto's af te laten drukken.
+                    Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto&apos;s die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto&apos;s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto&apos;s af te laten drukken.
                 </PricePackage>
 
                 <PricePackage
@@ -21,7 +21,7 @@ const PricePackages = () => {
                     uspList={["10 Foto's","Hoge kwaliteit nabewerkt", "Digitale download"]}
                     img="https://www.maxvanwijnen.nl/www/images/kinderen/Fotoshoot-strand-kind.webp"
                 >
-                    Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto's die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto\'s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto's af te laten drukken.
+                    Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto&apos;s die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto&apos;s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto&apos;s af te laten drukken.
                 </PricePackage>
                 <PricePackage
                     price={189}
@@ -29,7 +29,7 @@ const PricePackages = () => {
                     uspList={["15 Foto's","Hoge kwaliteit nabewerkt", "Digitale download"]}
                     img="https://www.maxvanwijnen.nl/www/images/kinderen/Fotoshoot-strand-kind.webp"
                 >
-                    Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto's die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto\'s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto's af te laten drukken.
+                    Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto&apos;s die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto&apos;s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto&apos;s af te laten drukken.
                 </PricePackage>
 
             </article>
