eaf9fe0a4babf3d63d1b7c0a7d33523f73bfb544
maxvanwijnen <maxvanwijnen@gmail.com>
2023-03-05 11:16:44 +0100

    pricepacakges

--- components/PricePackages/PricePackages.js
+++ components/PricePackages/PricePackages.js
@@ -2,35 +2,27 @@ import React from 'react';
 import css from './PricePackages.module.scss';
 import PricePackage from "./PricePackage/PricePackage";
 
-const PricePackages = () => {
+const PricePackages = ({pricePackages}) => {
+
+
+
     return (
         <section>
             <article className={css['price-packages']}>
-                <PricePackage
-                    price={89}
-                    type="basic"
-                    uspList={["5 Foto's","Hoge kwaliteit nabewerkt", "Digitale download"]}
-                    img="https://www.maxvanwijnen.nl/www/images/kinderen/Fotoshoot-strand-kind.webp"
-                >
-                    Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto&apos;s die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto&apos;s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto&apos;s af te laten drukken.
-                </PricePackage>
+                {
+                    pricePackages.map((pricePackage)=>{
+                        return ( <PricePackage
+                            key={pricePackage.type}
+                            price={pricePackage.price}
+                            type={pricePackage.type}
+                            uspList={pricePackage.usplist}
+                            img={pricePackage.img}
+                        >
+                            {pricePackage.desc}
+                        </PricePackage>)
+                    })
+                }
 
-                <PricePackage
-                    price={139}
-                    type="deluxe"
-                    uspList={["10 Foto's","Hoge kwaliteit nabewerkt", "Digitale download"]}
-                    img="https://www.maxvanwijnen.nl/www/images/kinderen/Fotoshoot-strand-kind.webp"
-                >
-                    Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto&apos;s die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto&apos;s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto&apos;s af te laten drukken.
-                </PricePackage>
-                <PricePackage
-                    price={189}
-                    type="premium"
-                    uspList={["15 Foto's","Hoge kwaliteit nabewerkt", "Digitale download"]}
-                    img="https://www.maxvanwijnen.nl/www/images/kinderen/Fotoshoot-strand-kind.webp"
-                >
-                    Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto&apos;s die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto&apos;s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto&apos;s af te laten drukken.
-                </PricePackage>
 
             </article>
         </section>
--- pages/fotoshoot/familie.js
+++ pages/fotoshoot/familie.js
@@ -45,7 +45,30 @@ export default function Familie (){
 
             </TextImg>
 
-            <PricePackages />
+            <PricePackages
+                pricePackages={
+                    [
+                        {price:89,
+                            type:'basic',
+                            usplist:["5 Foto's","Hoge kwaliteit nabewerkt", "Digitale download"],
+                            img:'https://www.maxvanwijnen.nl/www/docs/images/price-package/familieshoot-basic-fotoshoot.webp',
+                            desc:'Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto&apos;s die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto&apos;s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto&apos;s af te laten drukken.'
+                        },
+                        {price:139,
+                            type:'deluxe',
+                            usplist:["10 Foto's","Hoge kwaliteit nabewerkt", "Digitale download"],
+                            img:'https://www.maxvanwijnen.nl/www/docs/images/price-package/familieshoot-deluxe-fotoshoot.webp',
+                            desc:'Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto&apos;s die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto&apos;s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto&apos;s af te laten drukken.'
+                        },
+                        {price:189,
+                            type:'premium',
+                            usplist:["15 Foto's","Hoge kwaliteit nabewerkt", "Digitale download"],
+                            img:'https://www.maxvanwijnen.nl/www/docs/images/price-package/familieshoot-premium-fotoshoot.webp',
+                            desc:'Met het Basic fotopakket kunt u de magie en spontaniteit van de kindertijd vastleggen. Dit pakket biedt een korte, maar leuke fotosessie van uw kind, met vijf hoogwaardige, nabewerkte foto&apos;s die u na afloop eenvoudig download.  Met dit pakket krijgt u  een digitale download van uw foto&apos;s, zodat u ze gemakkelijk kunt delen met familie en vrienden. Maar het is ook mogelijk om foto&apos;s af te laten drukken.'
+                        }
+                    ]
+                }
+            />
 
             <ImageRow
                 images={[
