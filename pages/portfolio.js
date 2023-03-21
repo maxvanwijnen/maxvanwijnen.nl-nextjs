bb74847e0fbae3966712484ba74525f8caf99f36
maxvanwijnen <maxvanwijnen@gmail.com>
2023-03-21 20:47:34 +0100

    portfolio + textcontent width fix mobile

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactPhotoGallery from "../components/ReactPhotoGallery/ReactPhotoGallery";
+import TextContent from "../components/TextContent/TextContent";
 
 export default function Portfolio (){
     return (
         <section>
-            <div>Portfolio</div>
+            <TextContent
+                title="Mijn portfolio als fotograaf">Op deze pagina vindt je een selectie foto`s van eerdere shoots. Het juiste licht speelt een belangrijke factor bij het behalen van deze resultaten. Ik fotografeer daarom (afhankelijk van de locatie) het liefst ongeveer 2 uur na zonsopkomst / 2 uur voor zonsondergang. De zon staat dan mooi laag, het licht is zachter, en de schaduwen zijn mooi lang. Dat brengt de mooiste rustige achtergronden en zachte lichtval mijn modellen.
+            </TextContent>
             <ReactPhotoGallery />
         </section>
 
