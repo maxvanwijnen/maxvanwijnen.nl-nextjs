4ee3ee7a90713298cbe7558cd2466037547dfcac
maxvanwijnen <maxvanwijnen@gmail.com>
2023-04-02 11:05:23 +0200

    tst

--- components/ReactPhotoGallery/ReactPhotoGallery.js
+++ components/ReactPhotoGallery/ReactPhotoGallery.js
@@ -2,7 +2,7 @@ import React, { useState, useCallback } from "react";
 import Gallery from "react-photo-gallery";
 import Carousel, { Modal, ModalGateway } from "react-images";
 /*import { photos } from "./photos";*/
-
+//test
 
 function ReactPhotoGallery({photos}) {
     const [currentImage, setCurrentImage] = useState(0);
