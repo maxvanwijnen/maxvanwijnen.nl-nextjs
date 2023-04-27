defb1121c0b0967108c0968d29d00d65e3ccb14f
maxvanwijnen <maxvanwijnen@gmail.com>
2023-04-27 09:28:05 +0200

    analytics vercel

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 import Header from "../components/Header/Header.js";
 import { useRouter } from 'next/router';
 import Layout from '../components/layout';
-
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
 
@@ -10,6 +10,7 @@ function MyApp({ Component, pageProps }) {
             <Layout>
                 <Component {...pageProps} />
             </Layout>
+            <Analytics/>
 
 
             </>
