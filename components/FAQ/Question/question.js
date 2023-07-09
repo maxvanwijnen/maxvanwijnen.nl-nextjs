26c8d1a804e494ed3faee8deff042e0fddad445f
maxvanwijnen <maxvanwijnen@gmail.com>
2023-07-10 00:04:44 +0200

    collapsable faq question

--- components/FAQ/Question/question.js
+++ components/FAQ/Question/question.js
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import css from './question.module.scss';
+import { HiChevronRight } from "react-icons/hi";
 
 const Question = ({question, answer}) => {
+
+    const [extend,toggleExtend] = useState(false)
+
+    let cssClass = '';
+    if(extend){
+        cssClass = 'extended';
+    }
+    else {
+        cssClass = 'collapsed';
+    }
+
+
+
     return (
-        <div className={css['question']}>
-            <h3>{question}</h3>
-            <p>{answer}</p>
+        <div className={`${css['question']} ${css[cssClass]}`} onClick={()=>{toggleExtend(!extend)}}>
+            <h3>{question} <HiChevronRight className={css['arrow']} /></h3>
+            <p className={`${css[cssClass]}`}>{answer}</p>
         </div>
     )
 }
