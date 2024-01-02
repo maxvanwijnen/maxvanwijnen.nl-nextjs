28c4103e7473d29484d1b918c7c2d2b5e7044368
Max van Wijnen <maxvanwijnen@MacBook-Pro-van-Max.local>
2024-01-02 17:03:50 +0100

    google analytics

--- components/layout.js
+++ components/layout.js
@@ -2,7 +2,7 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import {useRouter} from "next/router";
 import breadCrumbStyles from '../styles/Breadcrumb.module.css';
-
+import { GoogleTagManager } from '@next/third-parties/google';
 import { AiFillHome } from 'react-icons/ai'
 import { PhotoshootTileMenu } from './../components/PhotoshootTileMenu/PhotoshootTileMenu';
 import WhatsAppChat from './WhatsAppChat/WhatsAppChat';
@@ -14,21 +14,23 @@ export default function Layout({ children , page}) {
 
     return (
         <>
-            <Header page={pathName} />
-            <main>
-                <section>
-                    {
-                        pathName && pathName != "fotoshoot" && <PhotoshootTileMenu isBookable={false} isBig={false} />
-                    }
+                <Header page={pathName} />
+                <main>
+                    <section>
+                        {
+                            pathName && pathName != "fotoshoot" && <PhotoshootTileMenu isBookable={false} isBig={false} />
+                        }
 
 
 
-                </section>
-                {children}
-                <WhatsAppChat />
-            </main>
-            <Footer/>
-
+                    </section>
+                    {children}
+                    <WhatsAppChat />
+                </main>
+                <Footer/>
+     
+            <GoogleTagManager gtmId="G-D580ZJDHNV" />
+            
         </>
     )
 }
