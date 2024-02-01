fe06fdb3b09fe68e51fd94db97c4c3e18f50edc9
Max van Wijnen <maxvanwijnen@MacBook-Pro-van-Max.local>
2024-02-01 18:38:58 +0100

    tilemenu zwangerschap

--- components/PhotoshootTileMenu/PhotoshootTileMenu.js
+++ components/PhotoshootTileMenu/PhotoshootTileMenu.js
@@ -22,7 +22,7 @@ export const PhotoshootTileMenu = ({isBig, isBookable, isPricePage}) => {
             <PhotoshootTile page="loveshoot" img="Koppel-fotoshoot-303.webp" size={size} isBookable={isBookable} isPricePage={isPricePage} />
             <PhotoshootTile page="portret" img="Portret-fotoshoot-303.webp" size={size} isBookable={isBookable} isPricePage={isPricePage} />
             <PhotoshootTile page="familie" img="Familie-fotoshoot-303.webp" size={size} isBookable={isBookable} isPricePage={isPricePage} />
-            <PhotoshootTile page="zwangerschap" img="Zwangerschaps-fotoshoot-303.webp" size={size} isBookable={isBookable} isPricePage={isPricePage} />
+            <PhotoshootTile page="zwangerschap" img="zwangerschap-fotoshoot2.webp" size={size} isBookable={isBookable} isPricePage={isPricePage} />
             <PhotoshootTile page="trouwen" img="Zwangerschaps-fotoshoot-303.webp" size={size} isBookable={isBookable} isPricePage={isPricePage} />
         </nav>
     )
