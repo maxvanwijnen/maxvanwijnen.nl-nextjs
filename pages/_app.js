ab2a423ab27d28380ac1cd392da1ff72b6fc5e16
Max van Wijnen <maxvanwijnen@MacBookPro.home>
2025-04-22 15:33:58 +0200

    fixed lcp blocking font

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,15 @@ import Header from "../components/Header/Header.js";
 import { useRouter } from 'next/router';
 import Layout from '../components/layout';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
 
   return  <>
+            <Script
+              src="https://fonts.googleapis.com/css2?family=Baloo+Tamma+2&family=Raleway&family=Roboto&family=Nunito:wght@600;900&display=swap"
+              strategy="afterInteractive"
+            />
             <Layout>
                 <Component {...pageProps} />
             </Layout>
@@ -17,5 +22,3 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp
-
-
