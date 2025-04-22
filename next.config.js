f8e269f8b26e9d27c05f9359557eee3b1dba2e69
Max van Wijnen <maxvanwijnen@MacBookPro.home>
2025-04-22 15:59:06 +0200

    speed improvement

--- next.config.js
+++ next.config.js
@@ -15,6 +15,19 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: '/insights/script.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable'
+          }
+        ]
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig;
