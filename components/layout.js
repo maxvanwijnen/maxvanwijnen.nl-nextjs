81e8fd9ab1d3f22538aa1e90e6368d484712a8a4
Max van Wijnen <maxvanwijnen@MacBookPro.home>
2025-04-22 16:12:23 +0200

    ga defer

--- components/layout.js
+++ components/layout.js
@@ -2,12 +2,11 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import {useRouter} from "next/router";
 import breadCrumbStyles from '../styles/Breadcrumb.module.css';
-import { GoogleTagManager } from '@next/third-parties/google';
 import { AiFillHome } from 'react-icons/ai'
 import { PhotoshootTileMenu } from './../components/PhotoshootTileMenu/PhotoshootTileMenu';
 import WhatsAppChat from './WhatsAppChat/WhatsAppChat';
 import CampaignPopUp from './CampaignPopUp/CampaignPopUp';
-
+import Script from 'next/script';
 
 export default function Layout({ children , page}) {
     const pathName = useRouter().asPath.replaceAll('/','');
@@ -29,7 +28,10 @@ export default function Layout({ children , page}) {
                 </main>
                 <Footer/>
      
-            <GoogleTagManager gtmId="GTM-WW67V52" />
+            <Script
+              src={`https://www.googletagmanager.com/gtm.js?id=GTM-WW67V52`}
+              strategy="afterInteractive"
+            />
             
         </>
     )
