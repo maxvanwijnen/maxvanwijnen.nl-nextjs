9f5c6c2f29a8463d1e07c6a8cb97b9c1b47524b6
Max van Wijnen <maxvanwijnen@MacBookPro.home>
2025-04-27 22:16:07 +0200

    verhoudingen afbeelding

--- components/TextImg/TextImg.js
+++ components/TextImg/TextImg.js
@@ -8,7 +8,7 @@ const TextImg = ({title,img, children}) => {
         <article className={css['text-img']}>
             <h2>{title}</h2>
             <div className={css['text-content']}>{children}</div>
-            <Image src={img} alt={title || ""} width={500} height={350} style={{width:'100%',maxWidth:'500px',borderRadius:'8px'}} />
+            <Image src={img} alt={title || ""} width={500} height={350} style={{width:'100%',maxWidth:'500px',borderRadius:'8px',height:'auto',objectFit:'cover'}} />
         </article>
         </>
     )
--- components/TxtImg2Column/TxtImg2Column.js
+++ components/TxtImg2Column/TxtImg2Column.js
@@ -8,22 +8,18 @@ export default function TxtImg2Column ( {children, txtTitle, imgSource, imgWidth
         flexDirection: isInverted ? 'row-reverse' : 'row'
     }
 
-
-
     return (
         <article className={styles['txtImg2Column']} style={varStyles}>
             <div className={styles['text']}>
-
                 {isFirstArticle ? <h1>{txtTitle}</h1> : <h2>{txtTitle}</h2> }
-                <div>
-                    {children}
-                </div>
+                <div>{children}</div>
             </div>
             <div className={styles['img-wrapper']}>
                 <Image src={imgSource}
                        width={imgWidth}
                        height={imgHeight}
                        alt={txtTitle}
+                       style={{ width: '100%', height: 'auto', maxWidth: '500px', borderRadius: '16px', objectFit: 'cover' }}
                 />
             </div>
         </article>
